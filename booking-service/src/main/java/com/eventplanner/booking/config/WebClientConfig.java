6a418cd25e50416be86a09bdc17dda9ccfc0afd3
Berkay <berkayulak0@gmail.com>
2026-01-11 15:56:27 +0300

    Update WebClientConfig.java

--- booking-service/src/main/java/com/eventplanner/booking/config/WebClientConfig.java
+++ booking-service/src/main/java/com/eventplanner/booking/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.eventplanner.booking.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -9,12 +10,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${event.service.base-url}")
+    private String eventServiceBaseUrl;
+
     @Bean
     public WebClient eventServiceClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("http://localhost:8082")
+                .baseUrl(eventServiceBaseUrl)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
 }
-////Önceden ayarlanmış bağlantı Bookingservice ve eventservice için (webclient)

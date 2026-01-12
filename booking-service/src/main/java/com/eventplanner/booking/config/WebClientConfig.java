cff7e5c65d3bd802df959083dfc1962e4b20405f
Berkay <berkayulak0@gmail.com>
2026-01-12 14:37:01 +0300

    Update WebClientConfig.java

--- booking-service/src/main/java/com/eventplanner/booking/config/WebClientConfig.java
+++ booking-service/src/main/java/com/eventplanner/booking/config/WebClientConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
-
+import org.springframework.beans.factory.annotation.Value;
 @Configuration
 public class WebClientConfig {
 
@@ -20,4 +20,5 @@ public WebClient eventServiceClient(WebClient.Builder builder) {
             .build();
 }
 }
+
 
